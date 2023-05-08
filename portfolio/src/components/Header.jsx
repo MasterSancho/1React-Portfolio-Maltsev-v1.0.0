ba13646870b1823da77ce72b04ceceb917ea289e
Alex Maltsev <57605252+MasterSancho@users.noreply.github.com>
2023-05-08 13:05:34 +0300

    001-->Main Header

---
+++ portfolio/src/components/Header.jsx
@@ -0,0 +1,101 @@
+import { Row, Col, Image, Button, Collapse } from 'react-bootstrap';
+import {
+ FaTwitter,
+ FaFacebook,
+ FaInstagram,
+ FaGithub,
+ FaHome,
+ FaGraduationCap,
+ FaFolderOpen,
+ FaEnvelope,
+} from 'react-icons/fa';
+
+const Header = () => {
+ return (
+  <header>
+   <Row className='nodutters'>
+    <Col lg={4} md={5}>
+     <Image src='assets/portrait.jpg' className='img' alt='image' />
+    </Col>
+    <Col lg={8} md={7}>
+     <div className='d-flex flex-column'>
+      <div className='p-5 bg-dark text-white'>
+       <div className='d-flex flex-row justify-content-between align-items-center'>
+        <h1 className='display-4'>John Doe</h1>
+        <div className='d-none d-md-block'>
+         <a href='http://twitter.com' className='text-white'>
+          <FaTwitter size={30} />
+         </a>
+        </div>
+        <div>
+         <a href='http://facebook.com' className='text-white'>
+          <FaFacebook size={30} />
+         </a>
+        </div>
+        <div>
+         <a href='http://instagram.com' className='text-white'>
+          <FaInstagram size={30} />
+         </a>
+        </div>
+        <div>
+         <a href='http://github.com' className='text-white'>
+          <FaGithub size={30} />
+         </a>
+        </div>
+       </div>
+      </div>
+
+      <div className='p-4 bg-black'>Experienced Full Stack Web Developer</div>
+
+      <div>
+       <div className='d-flex flex-row text-white align-items-stretch text-center'>
+        <Button
+         className='port-item p-4 bg-primary'
+         variant='primary'
+         data-toggle='collapse'
+         data-target='#home'
+         aria-expanded='false'
+         aria-controls='home'>
+         <FaHome size={120} className='d-block' />
+         <span className='d-none d-sm-block'>Home</span>
+        </Button>
+
+        <Button
+         className='port-item p-4 bg-success'
+         data-toggle='collapse'
+         data-target='#resume'
+         aria-expanded='false'
+         aria-controls='resume'>
+         <FaGraduationCap size={120} className='d-block' />
+         <span className='d-none d-sm-block'>Resume</span>
+        </Button>
+
+        <Button
+         className='port-item p-4 bg-warning'
+         data-toggle='collapse'
+         data-target='#work'
+         aria-expanded='false'
+         aria-controls='work'>
+         <FaFolderOpen size={120} className='d-block'></FaFolderOpen>
+         <span className='d-none d-sm-block'>Work</span>
+        </Button>
+
+        <Button
+         className='port-item p-4 bg-danger'
+         data-toggle='collapse'
+         data-target='#contact'
+         aria-expanded='false'
+         aria-controls='contact'>
+         <FaEnvelope size={120} className='d-block'></FaEnvelope>
+         <span className='d-none d-sm-block'>Contact</span>
+        </Button>
+       </div>
+      </div>
+     </div>
+    </Col>
+   </Row>
+  </header>
+ );
+};
+
+export default Header;
---
+++ portfolio/src/pages/HomePage.jsx
@@ -0,0 +1,13 @@
+import { Container } from 'react-bootstrap';
+import Header from '../components/Header';
+
+const HomePage = () => {
+ return (
+  <Container>
+   <Header />
+   <h1>Welcome To My Portfolio</h1>
+  </Container>
+ );
+};
+
+export default HomePage;
